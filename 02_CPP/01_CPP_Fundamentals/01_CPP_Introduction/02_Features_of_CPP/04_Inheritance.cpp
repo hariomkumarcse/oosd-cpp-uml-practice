52e09349562dc28ce6aa99b82392023e84d141c0
Hariom Kumar <hariomgok@gmail.com>
2026-09-22 21:38:34 +0530

    In 04_Inheritance.cpp file adding cpp code

---
+++ 02_CPP/01_CPP_Fundamentals/01_CPP_Introduction/02_Features_of_CPP/04_Inheritance.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+class Animal
+{
+	public:
+		void eat()
+		{
+			std::cout << "Animal is eating." << std::endl;
+		}
+};
+
+class Dog : public Animal
+{
+	public:
+		void bark()
+		{
+			std::cout << "Dog is barking." << std::endl;
+		}
+};
+
+int main()
+{
+	Dog dog;
+
+	dog.eat();
+	dog.bark();
+
+	return 0;
+}
